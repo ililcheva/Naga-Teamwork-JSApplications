16d901af571d95746176217d2499a01ac90aefbb
svrakata <svrakata@gmail.com>
2017-04-21 11:08:52 +0300

    structural changes

--- js/systemjsConf.js
+++ js/systemjsConf.js
@@ -1,8 +1,11 @@
 SystemJS.config({
     transpiler: 'plugin-babel',
     map: {
+        //libs
         'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+
+        //app scripts
         'main':'./js/main.js'
     }
 });
