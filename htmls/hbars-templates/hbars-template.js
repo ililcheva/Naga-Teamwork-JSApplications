e28b92a33ad901a1e071bede66ba7ebe157475cf
svrakata <svrakata@gmail.com>
2017-05-03 15:46:52 +0300

    search pattern completed

--- htmls/hbars-templates/hbars-template.js
+++ htmls/hbars-templates/hbars-template.js
@@ -9,18 +9,16 @@ const hbars = (function() {
     },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
         var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-        return "<div class=\"col-lg-3 col-md-4 col-xs-12\">\n    <div class=\"search-container\">\n        <img src=\""
+        return "<div class=\"col-lg-3 col-md-4 col-xs-12 \">\n    <div class=\"search-container result-div\">\n        <img src=\""
             + alias4(((helper = (helper = helpers.imageLink || (depth0 != null ? depth0.imageLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"imageLink","hash":{},"data":data}) : helper)))
             + "\" alt=\"book cover\" class=\"img-responsive img-thumbnail\">\n        <div class=\"info-container\">\n            <h4>"
             + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
             + "</h4>\n"
             + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.authors : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-            + "            <div>\n                "
-            + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
-            + "\n            </div>\n            <a href=\""
-            + alias4(((helper = (helper = helpers.infoLink || (depth0 != null ? depth0.infoLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"infoLink","hash":{},"data":data}) : helper)))
-            + "\">see on google books</a>\n        </div>\n    </div>\n</div>\n\n\n\n\n\n\n\n";
+            + "        </div>\n    </div>\n</div>\n";
     },"useData":true});
 })();
+
+
 
 export default hbars;
