842604b362e5937778a9efe6f749bd16f1943305
svrakata <svrakata@gmail.com>
2017-05-04 23:21:21 +0300

    styling for description added

--- htmls/hbars-templates/hbars-template.js
+++ htmls/hbars-templates/hbars-template.js
@@ -24,25 +24,24 @@ const hbars1 = (function() {
 const hbars2 = (function() {
     var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
     templates['description'] = template({"1":function(container,depth0,helpers,partials,data) {
-        return "        <h5>"
+        return "        <h2>"
             + container.escapeExpression(container.lambda(depth0, depth0))
-            + "</h5>\n";
+            + "</h2>\n";
     },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
         var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-        return "<div id=\"description-div\">\n    <h1>"
+        return "<div id=\"description-div\" >\n    <h1>"
             + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-            + "</h1>\n"
+            + "</h1>\n    <hr>\n"
             + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.authors : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-            + "    <h5>"
-            + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data}) : helper)))
-            + "</h5>\n    <h5>"
+            + "    <p>\n        "
             + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
-            + "</h5>\n    <h5>"
+            + "\n    </p>\n    <a href=\""
             + alias4(((helper = (helper = helpers.infoLink || (depth0 != null ? depth0.infoLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"infoLink","hash":{},"data":data}) : helper)))
-            + "</h5>\n    <button id=\"description-close\">CLOSE</button>\n</div>\n\n\n\n\n";
+            + "\" id=\"google-button\">Read on Google Books</a>\n    <button id=\"description-close\">X</button>\n</div>\n";
     },"useData":true});
 })();
+
 
 
 
