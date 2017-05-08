d918832758400793d542aa3365a592498236665b
al-nenov <al.nenov@gmail.com>
2017-05-08 07:46:19 +0300

    some tests added

--- tests/tests.js
+++ tests/tests.js
@@ -2,17 +2,57 @@ mocha.setup("bdd");
 //const chai = require("chai");
 const { expect } = chai;
 import dataBase from 'database';
+import events from 'events';
+
+describe("Events tests", () => {
+
+	it("Expect to be a signup form", () => {
+		let signForm = events.signUpForm;
+		expect(signForm).to.exist;
+	});
+
+	/*it("calls the original function", function () {
+
+		console.log(events.signUpForm)
+
+		let spyFunction = sinon.spy();
+
+		spyFunction();
+		spyFunction(2, 3, 4);
+		spyFunction(7, 6, [1, 'asdf', {}]);
+
+
+		function sum(...numbers) {
+			if (numbers.length === 0) {
+				throw 'No numbers';
+			}
+
+			return numbers.reduce((a, b) => a + b);
+		}
+
+		let s = sum(1, 6, 8, 9, 13);
+		console.log(s);
+
+		let sumSpied = sinon.spy(sum);
+
+		let obj = {};
+
+		sumSpied(4, 3, 2, 1);
+		try {
+			sumSpied();
+		}
+		catch (e) { }
+
+		sumSpied(1);
+
+		console.log(sumSpied.calledWithExactly(4, 3, 2, 7, 1));
+
+
+	});*/
+});
+
 
 describe("Login tests", () => {
-	const LOCAL_STORAGE_USERNAME_KEY = 'signed-in-user-username',
-		LOCAL_STORAGE_AUTHKEY_KEY = 'signed-in-user-auth-key';
-
-	const clearLocalStorage = () => {
-		localStorage.removeItem(LOCAL_STORAGE_USERNAME_KEY);
-		localStorage.removeItem(LOCAL_STORAGE_AUTHKEY_KEY);
-	};
-	beforeEach(clearLocalStorage);
-	afterEach(clearLocalStorage);
 
 	describe("User tests", () => {
 		describe("Register user", () => {
@@ -26,8 +66,8 @@ describe("Login tests", () => {
 			});
 			it('Expext create user to make a request', (done) => {
 				let user = {
-					email: "m",
-					password: "222"
+					email: "testovUser",
+					password: "Parola-123"
 				};
 				let response = {
 					result: {
@@ -64,6 +104,29 @@ describe("Login tests", () => {
 				expect(promise).to.be.an.instanceof(Promise);
 			});
 
+		});
+		describe("Login tests", () => {
+			it("Test if current user is set in localstorge", (done) => {
+				let loginUser = sinon.stub(dataBase, "loginUser");
+				const user = {
+					username: 'testuser',
+					password: '123456'
+				};
+
+				const response = {
+					result: {
+						username: user.username,
+						authKey: 'SOME_AUTH_KEY'
+					}
+				};
+				loginUser.returns(Promise.resolve(response));
+
+				loginUser(user)
+					.then(() => {
+						expect(localStorage.getItem('currentUser')).to.equal(user.username);
+					})
+					.then(done, done);
+			});
 		});
 
 	});
